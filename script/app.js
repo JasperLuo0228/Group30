21f7fd5fa8fece466cff443756dec92937de962a
Molata-Fu <junyifu@ucsb.edu>
2024-02-08 16:22:15 -0800

    website

---
+++ script/app.js
@@ -0,0 +1,11 @@
+function searchRoute() {
+    const start = document.getElementById('start').value;
+    const end = document.getElementById('end').value;
+    const option = document.getElementById('options').value;
+
+    // Perform mapping and routing based on selected option
+    // This is where you would call your mapping API or perform any other actions
+    console.log(`Mapping hitchhike from ${start} to ${end} with ${option} option`);
+    // Display map and route result on the webpage
+    // This is where you would update the map element with the route
+}
